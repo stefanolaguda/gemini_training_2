2add24225a458aff685dc752d33f2f4fa1ca177b
Stefano Laguda <s.laguda@hastega.it>
2025-01-15 11:12:11 +0100

    added base component for first exercise

---
+++ src/app/ui/pages/clothing-shop-1/clothing-shop-1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clothing-shop-1',
+  templateUrl: './clothing-shop-1.component.html',
+  styleUrl: './clothing-shop-1.component.scss'
+})
+export class ClothingShop1Component {
+
+}
---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClothingShop1Component } from './pages/clothing-shop-1/clothing-shop-1.component';
+
+
+
+@NgModule({
+  declarations: [
+    ClothingShop1Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UiModule { }
